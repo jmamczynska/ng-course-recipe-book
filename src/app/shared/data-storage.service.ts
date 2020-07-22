370eb0464ae3de62df9bca9db4315b911766c71d
Joanna Mamczynska <jmamczynska@objectivity.co.uk>
2020-07-22 20:06:40 +0200

    ATC-1: Http calls added to handle ingredients

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -3,30 +3,27 @@ import {HttpClient} from '@angular/common/http';
 import {RecipeService} from '../recipes/recipe.service';
 import {Recipe} from '../recipes/recipe.model';
 import {map, tap} from 'rxjs/operators';
-import {AuthService} from '../auth/auth.service';
 import {Observable} from 'rxjs';
 
 @Injectable()
 export class DataStorageService {
 
-  private readonly url = 'https://ng-course-recipe-book-69787.firebaseio.com/recipes.json';
+  private readonly recipesUrl = 'https://ng-course-recipe-book-69787.firebaseio.com/recipes.json';
 
   constructor(
       private httpClient: HttpClient,
-      private recipeService: RecipeService,
-      private authService: AuthService) {
+      private recipeService: RecipeService) {
   }
 
   storeRecipes() {
-    this.httpClient.put(this.url, this.recipeService.getRecipes())
+    this.httpClient.put(this.recipesUrl, this.recipeService.getRecipes())
         .subscribe(response => {
           console.log(response);
         });
   }
 
   getRecipes(): Observable<Recipe[]> {
-
-    return this.httpClient.get<Recipe[]>(this.url)
+    return this.httpClient.get<Recipe[]>(this.recipesUrl)
         .pipe(
             map((recipes: Recipe[]) => {
               return recipes.map(recipe => {
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -2,7 +2,6 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Ingredient} from '../shared/ingredient.model';
 import {ShoppingListService} from './shopping-list.service';
 import {Subscription} from 'rxjs';
-import {LoggingService} from '../logging.service';
 
 @Component({
   selector: 'app-shopping-list',
@@ -13,12 +12,12 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
   private ingredientsChangedSub: Subscription;
 
-  constructor(private shoppingListService: ShoppingListService, private loggingService: LoggingService) {
+  constructor(
+      private shoppingListService: ShoppingListService) {
   }
 
   ngOnInit() {
-    this.loggingService.printLog('Hello from ShoppingListComponent');
-    this.ingredients = this.shoppingListService.getIngredients();
+    this.shoppingListService.fetchIngredients();
     this.ingredientsChangedSub = this.shoppingListService.ingredientsChanged.subscribe(
         (ingredients: Ingredient[]) => {
           this.ingredients = ingredients;
@@ -32,5 +31,13 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
 
   onEditItem(index: number) {
     this.shoppingListService.startedEditing.next(index);
+  }
+
+  fetchShoppingList() {
+    this.shoppingListService.fetchIngredients();
+  }
+
+  deleteShoppingList() {
+    this.shoppingListService.removeAllIngredients();
   }
 }
