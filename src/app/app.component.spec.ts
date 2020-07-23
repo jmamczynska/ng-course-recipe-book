9efd508f1095466757946aa69ce2ed725aac2c20
Joanna Mamczynska <jmamczynska@objectivity.co.uk>
2020-07-23 14:14:33 +0200

    ATC-2: Unit tests fixed

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,41 @@
 import {async, TestBed} from '@angular/core/testing';
 
 import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AuthModule} from './auth/auth.module';
+import {LoggingService} from './logging.service';
+import {AuthService} from './auth/auth.service';
+import {DataStorageService} from './shared/data-storage.service';
+import SpyObj = jasmine.SpyObj;
+import createSpyObj = jasmine.createSpyObj;
 
 describe('AppComponent', () => {
+
+  let loggingService: SpyObj<LoggingService>;
+  let authService: SpyObj<AuthService>;
+  let dataStorageService: SpyObj<DataStorageService>;
+
   beforeEach(async(() => {
+
+    loggingService = createSpyObj(LoggingService.name, ['printLog']);
+    authService = createSpyObj(AuthService.name, ['autoLogin']);
+    dataStorageService = createSpyObj(DataStorageService.name, ['getRecipes', 'storeRecipes']);
+
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        HeaderComponent
       ],
+      imports: [
+        AppRoutingModule,
+        AuthModule
+      ],
+      providers: [
+        {provide: LoggingService, use: loggingService},
+        {provide: AuthService, use: authService},
+        {provide: DataStorageService, use: dataStorageService}
+      ]
     }).compileComponents();
   }));
 
@@ -15,18 +43,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  }));
-
-  it(`should have as title 'app works!'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
-  }));
-
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
   }));
 });
--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -1,22 +1,35 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {HeaderComponent} from './header.component';
+import {DataStorageService} from '../shared/data-storage.service';
+import {AuthService} from '../auth/auth.service';
+import SpyObj = jasmine.SpyObj;
+import createSpyObj = jasmine.createSpyObj;
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let dataStorageService: SpyObj<DataStorageService>;
+  let authService: SpyObj<AuthService>;
+
+  dataStorageService = createSpyObj(DataStorageService.name, ['getRecipes', 'storeRecipes']);
+  authService = createSpyObj(AuthService.name, ['logout']);
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [HeaderComponent]
-    })
-        .compileComponents();
+      declarations: [
+        HeaderComponent
+      ],
+      providers: [
+        {provide: DataStorageService, use: dataStorageService},
+        {provide: AuthService, use: authService}
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
